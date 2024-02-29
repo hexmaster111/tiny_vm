383febd1259f412ff7d2e6c490f4eb0b5aadc48f
hailey <_____@___.com>
2024-02-29 06:19:03 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#define MAX_STACK (20)
+
+int _stack[MAX_STACK] = {0};
+int _stackCurr = 0;
+
+int push(int v)
+{
+    if (_stackCurr >= MAX_STACK)
+        return 0;
+    _stack[_stackCurr] = v;
+    _stackCurr++;
+    return 1;
+}
+
+int isEmpty()
+{
+    return _stackCurr <= 0;
+}
+
+int pop(int *vptr)
+{
+    if (isEmpty())
+        return 0;
+    _stackCurr--;
+    *vptr = _stack[_stackCurr];
+    return 1;
+}
+
+int top(int *vptr)
+{
+
+    if (isEmpty())
+        return 0;
+    *vptr = _stack[_stackCurr];
+    return 1;
+}
+
+int size()
+{
+    return _stackCurr;
+}
+
+enum opp
+{
+    opp_push,
+    opp_add,
+    opp_print,
+};
+
+void Die()
+{
+    exit(1);
+}
+
+void parseline(char *line)
+{
+    char *token, *str, *tofree;
+
+    tofree = str = strdup(line); // We own str's memory now.
+    while ((token = strsep(&str, " ")))
+    {
+        // itterates through parts
+        fprintf(stdout, "token: %s\n", token);
+    }
+
+    free(tofree);
+}
+
+int main(int argc, char *argv[])
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    while (1)
+    {
+        fprintf(stdout, ">");
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+            break;
+
+        line[strlen(line) - 1] = '\0';
+        if (strcmp("exit", line) == 0)
+            break;
+
+        parseline(line);
+    }
+
+    free(line);
+    fprintf(stdout, "bye~\n");
+    return 0;
+}
---
+++ thoughts.md
@@ -0,0 +1,10 @@
+``` svm
+push 5
+push 10
+add
+print
+
+---output---
+15
+
+```
